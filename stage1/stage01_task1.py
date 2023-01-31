da99747c961157dab0c485f016b5cebd52ef7e3c
Marina_Unuchak <marina.unuchak@gmail.com>
2023-01-31 18:27:02 +0300

    added stage1 tasks

---
+++ stage1/stage01_task1.py
@@ -0,0 +1,20 @@
+# The program prints my favotite phrase
+#
+# Version 1.0
+# Author: marina.unuchak
+# Date: 23.01.2023
+
+print("My favorite phrase in English:")
+print("""
+    Wise men speak because they have something to say; 
+    fools because they have to say something.
+    ***
+    Platon
+""")
+print("Моя любимая цитата на русском:")
+print("""
+    Мудрые люди говорят потому, что им есть, что сказать, 
+    глупцы — потому что они должны сказать что-то. 
+    ***
+    Платон
+""")
